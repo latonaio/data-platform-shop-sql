76a565ba45ebe3ab174643a91cb5cdb3d60be877
misupopo <kokorononakaniitumo@yahoo.co.jp>
2024-05-07 16:15:15 +0900

    no message

--- data-platform-shop-sql-header-data.sql
+++ data-platform-shop-sql-header-data.sql
@@ -4,14 +4,21 @@ CREATE TABLE `data_platform_shop_header_data`
   `ShopType`                         varchar(4) NOT NULL,
   `ShopOwner`                        int(12) NOT NULL,
   `ShopOwnerBusinessPartnerRole`     varchar(40) NOT NULL,
+  `Brand`                            int(16) DEFAULT NULL,
   `PersonResponsible`                varchar(40) NOT NULL,
   `ValidityStartDate`                date NOT NULL,
   `ValidityStartTime`                time NOT NULL,
   `ValidityEndDate`                  date NOT NULL,
   `ValidityEndTime`                  time NOT NULL,
+  `DailyOperationStartTime           time NOT NULL,
+  `DailyOperationEndTime`            time NOT NULL,
   `Description`                      varchar(60) NOT NULL,
   `LongText`                         varchar(1000) NOT NULL,
   `Introduction`                     varchar(200) DEFAULT NULL,
+  `OperationRemarks`                 varchar(100) DEFAULT NULL,
+  `PhoneNumber`                      varchar(40) DEFAULT NULL,
+  `AvailabilityOfParking`            tinyint(1) DEFAULT NULL,
+  `NumberOfParkingSpaces`            int(4) DEFAULT NULL,
   `Site`                             int(16) NOT NULL,
   `Project`                          int(16) DEFAULT NULL,
   `WBSElement`                       int(8) DEFAULT NULL,
@@ -24,6 +31,8 @@ CREATE TABLE `data_platform_shop_header_data`
   `CreationTime`                     time NOT NULL,
   `LastChangeDate`                   date NOT NULL,
   `LastChangeTime`                   time NOT NULL,
+  `CreateUser`                       int(12) NOT NULL,
+  `LastChangeUser`                   int(12) NOT NULL,
   `IsReleased`                       tinyint(1) DEFAULT NULL,
   `IsMarkedForDeletion`              tinyint(1) DEFAULT NULL,
 
@@ -32,10 +41,13 @@ CREATE TABLE `data_platform_shop_header_data`
     CONSTRAINT `DPFMShopHeaderDataShopType_fk` FOREIGN KEY (`ShopType`) REFERENCES `data_platform_shop_type_shop_type_data` (`ShopType`),
     CONSTRAINT `DPFMShopHeaderDataShopOwner_fk` FOREIGN KEY (`ShopOwner`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
     CONSTRAINT `DPFMShopHeaderDataShopOwnerBPRole_fk` FOREIGN KEY (`ShopOwnerBusinessPartnerRole`) REFERENCES `data_platform_bp_role_bp_role_data` (`BusinessPartnerRole`),
+    CONSTRAINT `DPFMSiteHeaderDataBrand_fk` FOREIGN KEY (`Brand`) REFERENCES `data_platform_brand_header_data` (`Brand`),
     CONSTRAINT `DPFMShopHeaderDataSite_fk` FOREIGN KEY (`Site`) REFERENCES `data_platform_site_header_data` (`Site`),
     CONSTRAINT `DPFMShopHeaderDataProject_fk` FOREIGN KEY (`Project`) REFERENCES `data_platform_project_project_data` (`Project`),
     CONSTRAINT `DPFMShopHeaderDataWBSElement_fk` FOREIGN KEY (`Project`, `WBSElement`) REFERENCES `data_platform_project_wbs_element_data` (`Project`, `WBSElement`),
-    CONSTRAINT `DPFMShopHeaderDataPointConsumptionType_fk` FOREIGN KEY (`PointConsumptionType`) REFERENCES `data_platform_point_consumption_type_point_consumption_type_data` (`PointConsumptionType`)
+    CONSTRAINT `DPFMShopHeaderDataPointConsumptionType_fk` FOREIGN KEY (`PointConsumptionType`) REFERENCES `data_platform_point_consumption_type_point_consumption_type_data` (`PointConsumptionType`),
+    CONSTRAINT `DPFMShopHeaderDataCreateUser_fk` FOREIGN KEY (`CreateUser`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
+    CONSTRAINT `DPFMShopHeaderDataLastChangeUser_fk` FOREIGN KEY (`LastChangeUser`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
